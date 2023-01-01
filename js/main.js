5b1109134ab8befeb7fd4396201dc8c5602093f2
草 <hi@imgb.space>
2023-01-01 11:02:14 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+function writeLetter() {
+    var from1= document.getElementById("nameFrom").value;
+    var to1= document.getElementById("nameTo").value;
+    document.getElementById("fromName").innerHTML = from1;
+    document.getElementById("toName").innerHTML = to1;
+}
